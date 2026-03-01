38748cddf25d6d7261466df15c152381b650cc51
Daniel <874007+Mahagon@users.noreply.github.com>
2026-03-01 11:05:24 +0000

    chore: fix directory structure for helm-diff installation script
    Signed-off-by: Daniel <874007+Mahagon@users.noreply.github.com>

--- files/scripts/install-helm-diff.sh
+++ files/scripts/install-helm-diff.sh
@@ -1,4 +1,5 @@
 HELM_DIFF_VERSION="3.15.1"
-mkdir -p /usr/lib/helm/plugins/helm-diff/bin
+mkdir -p /usr/lib/helm/plugins/helm-diff
 curl -fsSL "https://github.com/databus23/helm-diff/releases/download/v${HELM_DIFF_VERSION}/helm-diff-linux-amd64.tgz" \
   | tar xz -C /usr/lib/helm/plugins/helm-diff
+chmod +x /usr/lib/helm/plugins/helm-diff/bin/helm-diff
