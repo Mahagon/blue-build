708c97c7ea6440858b3e2a888c8383a5f5642f76
Mahagon <874007+Mahagon@users.noreply.github.com>
2026-03-02 16:51:41 +0100

    chore: update install-prettier.sh to specify installation prefix for prettier
    Signed-off-by: Mahagon <874007+Mahagon@users.noreply.github.com>

--- files/scripts/install-prettier.sh
+++ files/scripts/install-prettier.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # renovate: datasource=npm depName=prettier
 PRETTIER_VERSION="3.8.1"
 
-npm install -g "prettier@${PRETTIER_VERSION}"
+npm install -g --prefix /usr "prettier@${PRETTIER_VERSION}"
