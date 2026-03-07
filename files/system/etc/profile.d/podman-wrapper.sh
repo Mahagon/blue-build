a22d50d60609695ff0c115f7dd927a7ab4065867
Mahagon <874007+Mahagon@users.noreply.github.com>
2026-03-07 18:50:06 +0100

    chore: add podman wrapper script to escape NoNewPrivileges constraints
    Signed-off-by: Mahagon <874007+Mahagon@users.noreply.github.com>

---
+++ files/system/etc/profile.d/podman-wrapper.sh
@@ -0,0 +1,7 @@
+# Wrap podman with systemd-run to escape NoNewPrivileges constraints
+# inherited from applications like VS Code (Electron sets NNP on itself,
+# which propagates to all child processes including the integrated terminal).
+podman() {
+    systemd-run --user --scope --quiet -- /usr/bin/podman "$@"
+}
+export -f podman
