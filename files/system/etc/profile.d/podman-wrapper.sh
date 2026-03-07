145ce10a1f09bed2aef2a1653b3bda45735a5d7e
Mahagon <874007+Mahagon@users.noreply.github.com>
2026-03-07 19:40:12 +0100

    chore: update podman wrapper to use --wait and --pipe options for NoNewPrivileges
    Signed-off-by: Mahagon <874007+Mahagon@users.noreply.github.com>

--- files/system/etc/profile.d/podman-wrapper.sh
+++ files/system/etc/profile.d/podman-wrapper.sh
@@ -2,6 +2,6 @@
 # inherited from applications like VS Code (Electron sets NNP on itself,
 # which propagates to all child processes including the integrated terminal).
 podman() {
-    systemd-run --user --scope --quiet -- /usr/bin/podman "$@"
+    systemd-run --user --wait --pipe --quiet -p NoNewPrivileges=no -- /usr/bin/podman "$@"
 }
 export -f podman
