1fdb6120c17f7ce95a98bc9034d749477abc2155
Mahagon <874007+Mahagon@users.noreply.github.com>
2026-03-17 10:55:09 +0100

    chore: refactor helm-docs installation script to improve file handling
    Signed-off-by: Mahagon <874007+Mahagon@users.noreply.github.com>

--- files/scripts/work/install-helm-docs.sh
+++ files/scripts/work/install-helm-docs.sh
@@ -6,5 +6,7 @@ HELM_DOCS_VERSION="1.14.2"
 
 echo "Installing helm-docs ${HELM_DOCS_VERSION}..."
 curl -fsSL "https://github.com/norwoodj/helm-docs/releases/download/v${HELM_DOCS_VERSION}/helm-docs_${HELM_DOCS_VERSION}_Linux_x86_64.tar.gz" \
-  | tar xz -C /usr/bin helm-docs
+  -o /tmp/helm-docs.tar.gz
+tar -xzf /tmp/helm-docs.tar.gz -C /usr/bin helm-docs
 chmod +x /usr/bin/helm-docs
+rm -f /tmp/helm-docs.tar.gz
