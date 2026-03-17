bf14e45f6218cb3f76eaed882cfd8419fd952ecc
Mahagon <874007+Mahagon@users.noreply.github.com>
2026-03-17 10:55:18 +0100

    chore: refactor terraform-docs installation script for improved file handling
    Signed-off-by: Mahagon <874007+Mahagon@users.noreply.github.com>

--- files/scripts/work/install-terraform-docs.sh
+++ files/scripts/work/install-terraform-docs.sh
@@ -6,5 +6,7 @@ TERRAFORM_DOCS_VERSION="0.21.0"
 
 echo "Installing terraform-docs ${TERRAFORM_DOCS_VERSION}..."
 curl -fsSL "https://github.com/terraform-docs/terraform-docs/releases/download/v${TERRAFORM_DOCS_VERSION}/terraform-docs-v${TERRAFORM_DOCS_VERSION}-linux-amd64.tar.gz" \
-  | tar xz -C /usr/bin terraform-docs
+  -o /tmp/terraform-docs.tar.gz
+tar -xzf /tmp/terraform-docs.tar.gz -C /usr/bin terraform-docs
 chmod +x /usr/bin/terraform-docs
+rm -f /tmp/terraform-docs.tar.gz
