2f2cd627b93beea1f49475a5cbbfbe3a4dd6330c
Mahagon <874007+Mahagon@users.noreply.github.com>
2026-03-28 16:51:23 +0100

    refactor k9s functions
    Signed-off-by: Mahagon <874007+Mahagon@users.noreply.github.com>

--- files/work/etc/profile.d/k9s-functions.sh
+++ files/work/etc/profile.d/k9s-functions.sh
@@ -2,26 +2,25 @@
 
 k9saws() {
   local environment=${1:-predev}
-  if [[ $environment != dev && $environment != predev ]]; then
-    _K9SAWS_CLEANUP_ENV="$environment"
-    trap 'cleanup_k8s_aws_credentials "$_K9SAWS_CLEANUP_ENV"' EXIT
-  fi
-  get_k8s_aws_credentials "$environment"
-  EDITOR="code --wait" /usr/bin/k9s --context "zvoove-$environment-cluster" "${@:2}"
+  (
+    if [[ $environment != dev && $environment != predev ]]; then
+      trap 'cleanup_k8s_aws_credentials "$environment"' EXIT
+    fi
+    get_k8s_aws_credentials "$environment"
+    EDITOR="code --wait" /usr/bin/k9s --context "zvoove-$environment-cluster" "${@:2}"
+  )
 }
 
 k9saz() {
   local environment=${1:-predev}
-  if [[ $environment != dev && $environment != predev ]]; then
-    _K9SAZ_CLEANUP_ENV="$environment"
-    trap 'cleanup_k8s_az_credentials "$_K9SAZ_CLEANUP_ENV"' EXIT
-  fi
-  # shellcheck disable=SC2034 -- used via dynamic scoping in get_k8s_az_credentials
-  local resourcegroup
-  resourcegroup=$(yq -r .terraformStateResourceGroup "$HOME/Documents/src/zvoove-SaaS/pipelines/saas-infrastructure/environment/${environment}.yaml")
-  (az aks list || az login) &> /dev/null
-  get_k8s_az_credentials "$environment"
-  EDITOR="code --wait" /usr/bin/k9s "${@:2}"
+  (
+    if [[ $environment != dev && $environment != predev ]]; then
+      trap 'cleanup_k8s_az_credentials "$environment"' EXIT
+    fi
+    _az_ensure_login
+    get_k8s_az_credentials "$environment"
+    EDITOR="code --wait" /usr/bin/k9s "${@:2}"
+  )
 }
 
 if [[ -n "${BASH_VERSION:-}" ]]; then
