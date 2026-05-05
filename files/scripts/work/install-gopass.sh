2952142060fc9f9a078bf0ce9269d307976f4767
Mahagon <874007+Mahagon@users.noreply.github.com>
2026-05-05 09:27:08 +0200

    chore: update checksum grep pattern in gopass installation script
    Signed-off-by: Mahagon <874007+Mahagon@users.noreply.github.com>

--- files/scripts/work/install-gopass.sh
+++ files/scripts/work/install-gopass.sh
@@ -9,7 +9,7 @@ curl -fsSL "https://github.com/gopasspw/gopass/releases/download/v${GOPASS_VERSI
   -o /tmp/gopass.tar.gz
 curl -fsSL "https://github.com/gopasspw/gopass/releases/download/v${GOPASS_VERSION}/gopass_${GOPASS_VERSION}_SHA256SUMS" \
   -o /tmp/gopass-checksums.txt
-echo "$(grep "gopass-${GOPASS_VERSION}-linux-amd64.tar.gz" /tmp/gopass-checksums.txt | awk '{print $1}')  /tmp/gopass.tar.gz" \
+echo "$(grep "gopass-${GOPASS_VERSION}-linux-amd64.tar.gz$" /tmp/gopass-checksums.txt | awk '{print $1}')  /tmp/gopass.tar.gz" \
   | sha256sum --check
 tar xzf /tmp/gopass.tar.gz -C /usr/bin gopass
 chmod +x /usr/bin/gopass
